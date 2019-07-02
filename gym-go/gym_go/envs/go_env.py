140397dd97b2075c0c2944aa98a71df4d28bb1ed
Jenny Chen <xc14@illinois.edu>
2019-07-01 21:11:06 -0700

    General structure fore step function

--- gym-go/gym_go/envs/go_env.py
+++ gym-go/gym_go/envs/go_env.py
@@ -16,9 +16,10 @@ class GoEnv(gym.Env):
 
     def __init__(self, size='S', reward_method='heuristic'):
         '''
-        @param reward_method: either 'heuristic' or 'real', 'heuristic' 
-            gives # your pieces - # oppo pieces. 'real' gives 0 for in-game
-            move, 1 for winning, -1 for losing, 0 for draw.
+        @param reward_method: either 'heuristic' or 'real' 
+        heuristic: gives # black pieces - # white pieces. 
+        real: gives 0 for in-game move, 1 for winning, -1 for losing, 
+            0 for draw, all from black player's perspective
         '''
         # determine board size
         try:
@@ -37,6 +38,11 @@ class GoEnv(gym.Env):
 
         # black goes first
         self.curr_player = 'b'
+
+        self.prev_player_passed = False
+
+        # whether the game is done
+        self.done = False
         
 
     def print_state(self):
@@ -55,35 +61,95 @@ class GoEnv(gym.Env):
         return observation, reward, done, info 
         '''
         
-        # sanity check for the move
-        if self.go_board.is_move_on_board(action):
-            self.print_state()
-            raise Exception('Move {} is illegal: there is already a piece at this location'.format(action))
-        if self.go_board.is_move_suicide(self.curr_player, action):
-            self.print_state()
-            raise Exception('Move {} is illegal: this move is suicide'.format(action))
-        if self.go_board.is_simple_ko(self.curr_player, action):
-            self.print_state()
-            raise Exception('Move {} is illegal: this location is a Ko'.format(action))
-
         # update whether previous player have passed
         self.board_info[3].fill(int(self.prev_player_passed))
 
-        # check if the current player passes
+        # if the current player passes
         if action is None:
-            self.update_board_info()
-            self.prev_player_passed = True
-            return self.board_info
-        else:
-            self.prev_player_passed = False
+            # if two consecutive passes, game is over
+            if self.prev_player_passed:
+                self.done = True
 
-        # TODO add apply_move
+        # the current player makes a move
+        else:
+            # make sure the move is legal
+            self.check_legal_move(action)
+
+            # make the move and extract board info
+            self.go_board.apply_move(self.curr_player, action)
+            self.update_board_info()
+
+            # switch player for the next round
+            self.curr_player = self.go_board.other_color(self.curr_player)
+
+        # store whether this player passed
+        self.prev_player_passed = action is None
+
+        return self.board_info, self.get_reward(), self.done, None
+
+
+    def check_legal_move(self, action):
+        '''
+        Check: piece already on board, move is suicide, move is on ko
+            move is out of board.
+        '''
+        exception_prefix = 'Move {} is illegal: '.format(action)
+
+        # sanity check for the move
+        if self.go_board.is_move_on_board(action):
+            self.print_state()
+            raise Exception(exception_prefix + 'there is already a piece at this location')
+        if self.go_board.is_move_suicide(self.curr_player, action):
+            self.print_state()
+            raise Exception(exception_prefix + 'this move is suicide')
+        if self.go_board.is_simple_ko(self.curr_player, action):
+            self.print_state()
+            raise Exception(exception_prefix + 'this location is a Ko')
+        if not self.is_move_within_bounds(action):
+            raise Exception(exception_prefix + 'out of board')
+
+
+    def is_move_within_bounds(self, action):
+        return action[0] >= 0 and action[0] < self.board_width \
+            and action[1] >= 0 and action[1] < self.board_width
+
+
+    def get_reward(self):
+        '''
+        Return reward based on reward_method.
+        heuristic: black total area - white total area
+        real: 0 for in-game move, 1 for winning, -1 for losing, 
+            0 for draw, from black player's perspective.
+            Winning and losing based on the Area rule
+        Win definition: https://en.wikipedia.org/wiki/Rules_of_Go#End
+        '''
+        if self.reward_method == 'real':
+            if self.done:
+                final_reward = self.get_area_reward() 
+
+                if final_reward == 0:
+                    return 0
+                elif final_reward > 0:
+                    return 1
+                else:
+                    return -1
+            else: 
+                return 0
+
+        elif self.reward_method == 'heuristic':
+            return self.get_area_reward()
+
+        else:
+            raise Exception('Unsupported reward method: {}'.format(self.reward_method))
+
+    def get_area_reward(self):
+        pass
+        
 
     def update_board_info(self):
         '''
         Update Black, white, Ko
         '''
-
         # reset board info
         self.board_info.fill(0)
 
