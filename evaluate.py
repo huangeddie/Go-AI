4cb7a1b99a6c910fb86740fee83726c378807c88
eddie <aigagror@gmail.com>
2019-10-30 14:59:06 -0500

    temperature tweaking

--- evaluate.py
+++ evaluate.py
@@ -13,15 +13,14 @@ go_env = gym.make('gym_go:go-v0', size=args.boardsize)
 # Policies
 checkpoint_model = value_models.ValueNet(args.boardsize)
 checkpoint_model.load_state_dict(torch.load(args.check_path))
-checkpoint_pi = policies.MCTS('Checkpoint', checkpoint_model, 0, args.temp)
+checkpoint_pi = policies.MCTS('Checkpoint', checkpoint_model, args.mcts, args.temp)
 print("Loaded model")
 
 # Sample trajectory and plot prior qvals
-# metrics.plot_traj_fig(go_env, checkpoint_pi, f'episodes/atraj_{checkpoint_pi.temp:.5f}.pdf')
-# print("Plotted sample trajectory")
+metrics.plot_traj_fig(go_env, checkpoint_pi, f'episodes/atraj_{checkpoint_pi.temp:.5f}.pdf')
+print("Plotted sample trajectory")
 
 # Play
 checkpoint_pi.set_temp(0)
-checkpoint_pi.num_searches = args.mcts
 go_env.reset()
 game.pit(go_env, policies.HUMAN_PI, checkpoint_pi, False)
