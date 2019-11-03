1547c873f1646a3aa32b704356c3173df3d502ce
eddie <aigagror@gmail.com>
2019-11-03 09:51:01 -0600

    temperature tweaking

--- utils.py
+++ utils.py
@@ -18,8 +18,8 @@ def hyperparameters():
     parser.add_argument('--boardsize', type=int, help='board size')
     parser.add_argument('--mcts', type=int, default=0, help='monte carlo searches')
 
-    parser.add_argument('--temp', type=float, default=1 / 16, help='initial temperature')
-    parser.add_argument('--tempsteps', type=float, default=8, help='first k steps to apply temperature to pi')
+    parser.add_argument('--temp', type=float, default=1 / 32, help='initial temperature')
+    parser.add_argument('--tempsteps', type=float, default=16, help='first k steps to apply temperature to pi')
 
     parser.add_argument('--batchsize', type=int, default=64, help='batch size')
     parser.add_argument('--replaysize', type=int, default=400000, help='replay memory size')
