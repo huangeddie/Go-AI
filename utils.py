cd6326e292fc99f99a65a4f5228e548f3bce1f19
eddie <aigagror@gmail.com>
2019-12-14 13:42:00 -0600

    added comments to hyperparams

--- utils.py
+++ utils.py
@@ -17,36 +17,47 @@ def hyperparameters():
     today = str(datetime.date.today())
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--checkpoint', type=bool, default=False, help='continue from checkpoint')
 
+    # Go Environment
     parser.add_argument('--boardsize', type=int, default=9, help='board size')
+    parser.add_argument('--reward', type=str, choices=['real', 'heuristic'], default='real', help='reward system')
+
+    # Monte Carlo Tree Search
     parser.add_argument('--mcts', type=int, default=0, help='monte carlo searches')
     parser.add_argument('--branches', type=int, default=4, help='branch degree for searching')
     parser.add_argument('--depth', type=int, default=3, help='search depth')
 
-    parser.add_argument('--temp', type=float, default=1 / 10, help='initial temperature')
-    parser.add_argument('--tempsteps', type=float, default=8, help='first k steps to apply temperature to pi')
+    # Learning Parameters
     parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
 
+    # Exploration
+    parser.add_argument('--temp', type=float, default=1 / 10, help='initial temperature')
+    parser.add_argument('--tempsteps', type=float, default=8, help='first k steps to apply temperature to pi')
+
+    # Data Sizes
     parser.add_argument('--batchsize', type=int, default=32, help='batch size')
     parser.add_argument('--replaysize', type=int, default=200000, help='max replay memory size')
     parser.add_argument('--trainsize', type=int, default=1000 * 32, help='train data size for one iteration')
 
+    # Training
+    parser.add_argument('--checkpoint', type=bool, default=False, help='continue from checkpoint')
     parser.add_argument('--iterations', type=int, default=128, help='iterations')
     parser.add_argument('--episodes', type=int, default=32, help='episodes')
     parser.add_argument('--evaluations', type=int, default=32, help='episodes')
     parser.add_argument('--eval-interval', type=int, default=1, help='iterations per evaluation')
 
+    # Disk Data
     parser.add_argument('--episodesdir', type=str, default='bin/episodes/', help='directory to store episodes')
     parser.add_argument('--savedir', type=str, default=f'bin/baselines/{today}/')
     parser.add_argument('--basepath', type=str, default=f'bin/{today}/base.pt', help='model path for baseline model')
 
+    # Model
     parser.add_argument('--agent', type=str, choices=['mcts', 'ac', 'mcts-ac'], default='mcts', help='type of agent/model')
     parser.add_argument('--baseagent', type=str, choices=['mcts', 'ac', 'mcts-ac', 'rand', 'greedy', 'human'],
         default='rand', help='type of agent/model for baseline')
-    parser.add_argument('--reward', type=str, choices=['real', 'heuristic'], default='real', help='reward system')
     parser.add_argument('--resblocks', type=int, default=4, help='number of basic blocks for resnets')
 
+    # Hardware
     parser.add_argument('--device', type=str, choices=['cpu', 'cuda'], default='cpu', help='device for pytorch models')
 
     return parser.parse_args()
